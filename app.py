3ca07b4ba38a2587ae01a59b52b90bf81ca247dc
eshaan <eshaan428@gmail.com>
2021-11-27 18:11:17 +0530

    New files updated

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import re
 from flask import Flask
 from flask.templating import render_template
 from flask.helpers import url_for
+from flask import request
 from werkzeug.utils import redirect
+import pandas
 
 app = Flask(__name__)
 
@@ -16,6 +19,16 @@ def customerseg():
 @app.route("/contactus")
 def contact():
     return render_template("contactUs.html")
+
+@app.route("/customer-rfm-analysis/user", methods = ['POST'])
+def user():
+    if request.method  == 'POST':
+        ID = request.form["Id"]
+    print("Customer ID is: ", ID)
+
+    
+    return render_template("customer.html")
+
 
 if __name__ == "__main__":
     app.run(debug = True, port = 8080)
